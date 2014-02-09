8405356916420aef05b533431352fefe19a50ae5
Darryl F <darryl@tzap.ca>
2014-02-08 19:58:47 -0800

    Fix up uri and version.

--- workshop_attendance.php
+++ workshop_attendance.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Workshop Attendance
-Plugin URI: http://www.thecarnivalband.com
+Plugin URI: https://github.com/OpenAirOrchestra/workshop_attendance
 Description: A simple workshop attendance plugin for the carnival band
-Version: 1.1
+Version: 1.2
 Author: DarrylF
 Author URI: http://www.thecarnivalband.com
 License: GPL2
