af6d3ff4bd5374bb691c70476687f7389a32e159
Darryl F <darryl@tzap.ca>
2021-12-08 16:29:49 -0800

    even odd colouring

--- take_attendance/src/SearchBar.js
+++ take_attendance/src/SearchBar.js
@@ -8,9 +8,9 @@ function SearchBar(props) {
       <form>
         <input type="text" name="search" placeholder="&#128269;"/>
 	<br/>
-        <input type="checkbox" name="recent"/><label for="recent">recent</label>
-        <input type="checkbox" name="new"/><label for="new">new folks</label>
-        <input type="checkbox" name="attendees"/><label for="attendees">attendees</label>
+        <input type="checkbox" name="recent" id="recent"/><label for="recent">recent</label>
+        <input type="checkbox" name="new" id="new"/><label for="new">new folks</label>
+        <input type="checkbox" name="attendees" id="attendees"/><label for="attendees">attendees</label>
       </form>
     </div>      
   )
