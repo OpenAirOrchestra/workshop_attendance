b37c28022c58c950b83928a1aa7a86d6f37337f9
Darryl F <darryl@tzap.ca>
2021-12-08 16:36:54 -0800

    Yikes, for got Header.js

---
+++ take_attendance/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default Header
+
+function Header(props) {
+  return (
+    <div className='Header'> 
+     <h1>Attendance for {props.name}</h1> 
+    </div>      
+  )
+}
