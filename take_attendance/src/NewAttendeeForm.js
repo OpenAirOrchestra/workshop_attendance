f559489ca9b69c8b8fe4ea6dd3554bbdaea85dc1
Darryl F <darryl@tzap.ca>
2021-12-12 10:50:48 -0800

    enable add button when required fields populated

--- take_attendance/src/NewAttendeeForm.js
+++ take_attendance/src/NewAttendeeForm.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default NewAttendeeForm
 
 function NewAttendeeForm(props) {
 
   const hideAttendeeForm = props.hideAttendeeForm;
+
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
 
   if (hideAttendeeForm) {
     return null;
@@ -14,11 +17,11 @@ function NewAttendeeForm(props) {
     <div className='NewAttendeeForm'>
       <h2>Add New Attendee</h2>
       <form>
-        <label htmlFor="firstname">First Name:</label>
-        <input type="text" name="firstname" id="firstname" />
+        <label htmlFor="firstname">First Name (required):</label>
+        <input type="text" name="firstname" id="firstname" className='required' value={firstname} onChange={(event) => setFirstname(event.target.value)}/>
         <br />
-        <label htmlFor="lastname">Last Name: </label>
-        <input type="text" name="lastname" id="lastname" />
+        <label htmlFor="lastname">Last Name (required): </label>
+        <input type="text" name="lastname" id="lastname" className='required' value={lastname} onChange={(event) => setLastname(event.target.value)}/>
         <br />
         <label htmlFor="email">Email:</label>
         <input type="text" name="email" id="email" />
@@ -31,7 +34,7 @@ function NewAttendeeForm(props) {
         <textarea name="notes" id="notes" />
         <br />
         <div className='centered'>
-          <input type="submit" value="Add" />
+          <input type="submit" value="Add" disabled={!(firstname && lastname)} />
         </div>
       </form>
     </div>
