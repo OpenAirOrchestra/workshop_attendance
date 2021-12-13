87fca3a1b430a211b47dc10e5e89aa2ad55dd576
Darryl F <darryl@tzap.ca>
2021-12-13 15:53:35 -0800

    More interesting names.

--- src/take_attendance/src/MockAttendanceService.js
+++ src/take_attendance/src/MockAttendanceService.js
@@ -9,21 +9,21 @@ class MockAttendanceService {
     constructor() {
         this.attendanceRecords = [
             { user_id: 99, firstname: 'Zaphod', lastname: 'Beeblebrox', phone: '', email: '', notes: 'Presedent of the Galaxy', event_id: 300, id: 199 },
-            { user_id: 100, firstname: 'Alice', lastname: 'Dole', phone: '', email: '', notes: 'A Tuba', event_id: 1000, id: 200 },
-            { user_id: 101, firstname: 'Bob', lastname: 'Gill', phone: '', email: '', notes: 'Bob is a fella', event_id: 1000, id: 201 },
-            { user_id: 102, firstname: 'Charlie', lastname: 'Lipp', phone: '', email: '', notes: 'C Saxophone', event_id: 1000, id: 202 },
-            { user_id: 103, firstname: 'Denise', lastname: 'Stephan', phone: '', email: '', notes: '', event_id: 1000, id: 203 },
-            { user_id: 104, firstname: 'Ethan', lastname: 'Fuller', phone: '', email: '', notes: 'F Flute', event_id: 1000, id: 204 },
+            { user_id: 100, firstname: 'Alice', lastname: 'Atalan', phone: '', email: '', notes: 'A Tuba', event_id: 1000, id: 200 },
+            { user_id: 101, firstname: 'Bob', lastname: 'Shamilov', phone: '', email: '', notes: 'Bob is a fella', event_id: 1000, id: 201 },
+            { user_id: 102, firstname: 'Charlie', lastname: 'Khalil', phone: '', email: '', notes: 'C Saxophone', event_id: 1000, id: 202 },
+            { user_id: 103, firstname: 'Denise', lastname: 'Usoyan', phone: '', email: '', notes: '', event_id: 1000, id: 203 },
+            { user_id: 104, firstname: 'Ethan', lastname: 'Adi', phone: '', email: '', notes: 'F Flute', event_id: 1000, id: 204 },
 
-            { user_id: 105, firstname: 'Francine', lastname: 'Churchill', phone: '', email: '', notes: 'C Flute', id: 205 },
-            { user_id: 106, firstname: 'Greg', lastname: 'Guard', phone: '', email: '', notes: 'Clarinet', id: 206 },
-            { user_id: 107, firstname: 'Harry', lastname: 'Gill', phone: '', email: '', notes: 'Bassoon', id: 207 },
+            { user_id: 105, firstname: 'Francine', lastname: 'Shavershian', phone: '', email: '', notes: 'C Flute', id: 205 },
+            { user_id: 106, firstname: 'Greg', lastname: 'Mori', phone: '', email: '', notes: 'Clarinet', id: 206 },
+            { user_id: 107, firstname: 'Harry', lastname: 'Tamoyan', phone: '', email: '', notes: 'Bassoon', id: 207 },
             { user_id: 108, firstname: 'Ichabod', lastname: 'Crane', phone: '', email: '', notes: 'Trombone', id: 208 },
 
             { user_id: 110, firstname: 'Niel', lastname: 'Armstrong', phone: '', email: '', notes: 'Astronaut', event_id: 10, id: 300 },
-            { user_id: 111, firstname: 'Oprah', lastname: 'Winfrey', phone: '', email: '', notes: 'Celebrety', event_id: 10, id: 310 },
-            { user_id: 112, firstname: 'Peter', lastname: 'Armguister', phone: '', email: '', notes: 'Dude', event_id: 10, id: 320 },
-            { user_id: 113, firstname: 'Quinn', lastname: 'Eskimo', phone: '', email: '', notes: 'Beatles', event_id: 10, id: 330 },
+            { user_id: 111, firstname: 'Oprah', lastname: 'Khario', phone: '', email: '', notes: 'Celebrety', event_id: 10, id: 310 },
+            { user_id: 112, firstname: 'Peter', lastname: 'Serdar', phone: '', email: '', notes: 'Dude', event_id: 10, id: 320 },
+            { user_id: 113, firstname: 'Quinn', lastname: 'Evdal', phone: '', email: '', notes: 'Beatles', event_id: 10, id: 330 },
 
             { firstname: 'Sonny', lastname: 'Bono', phone: '', email: '', notes: 'Celebrity husband', event_id: 1000, id: 340 },
             { firstname: 'Frank', lastname: 'Zappa', phone: '', email: '', notes: 'Guitar', event_id: 1004, id: 350 }
