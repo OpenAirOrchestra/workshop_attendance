d03de72b5e3f0bc202ce47e0146c85e586c314f6
Darryl F <darryl@tzap.ca>
2021-12-15 14:53:54 -0800

    First light of day querying workshop from database.

--- controllers/workshop_rest_controller.php
+++ controllers/workshop_rest_controller.php
@@ -1,35 +1,35 @@
 <?php
- 
-class WorkshopRestController extends WP_REST_Controller {
- 
+
+class WorkshopRestController extends WP_REST_Controller
+{
+
   /**
    * Register the routes for the objects of the controller.
    */
-  public function register_routes() {
+  public function register_routes()
+  {
     $version = '1';
     $namespace = 'workshop_attendance/v' . $version;
     $base = 'event';
-    register_rest_route( $namespace, '/' . $base, array(
+    register_rest_route($namespace, '/' . $base, array(
       array(
         'methods'             => WP_REST_Server::READABLE,
-        'callback'            => array( $this, 'get_items' ),
-        'permission_callback' => array( $this, 'get_items_permissions_check' ),
-        'args'                => array(
- 
-        ),
+        'callback'            => array($this, 'get_items'),
+        'permission_callback' => array($this, 'get_items_permissions_check'),
+        'args'                => array(),
       ),
       array(
         'methods'             => WP_REST_Server::CREATABLE,
-        'callback'            => array( $this, 'create_item' ),
-        'permission_callback' => array( $this, 'create_item_permissions_check' ),
-        'args'                => $this->get_endpoint_args_for_item_schema( true ),
+        'callback'            => array($this, 'create_item'),
+        'permission_callback' => array($this, 'create_item_permissions_check'),
+        'args'                => $this->get_endpoint_args_for_item_schema(true),
       ),
-    ) );
-    register_rest_route( $namespace, '/' . $base . '/(?P<id>[\d]+)', array(
+    ));
+    register_rest_route($namespace, '/' . $base . '/(?P<id>[\d]+)', array(
       array(
         'methods'             => WP_REST_Server::READABLE,
-        'callback'            => array( $this, 'get_item' ),
-        'permission_callback' => array( $this, 'get_item_permissions_check' ),
+        'callback'            => array($this, 'get_item'),
+        'permission_callback' => array($this, 'get_item_permissions_check'),
         'args'                => array(
           'context' => array(
             'default' => 'view',
@@ -38,182 +38,204 @@ class WorkshopRestController extends WP_REST_Controller {
       ),
       array(
         'methods'             => WP_REST_Server::EDITABLE,
-        'callback'            => array( $this, 'update_item' ),
-        'permission_callback' => array( $this, 'update_item_permissions_check' ),
-        'args'                => $this->get_endpoint_args_for_item_schema( false ),
+        'callback'            => array($this, 'update_item'),
+        'permission_callback' => array($this, 'update_item_permissions_check'),
+        'args'                => $this->get_endpoint_args_for_item_schema(false),
       ),
       array(
         'methods'             => WP_REST_Server::DELETABLE,
-        'callback'            => array( $this, 'delete_item' ),
-        'permission_callback' => array( $this, 'delete_item_permissions_check' ),
+        'callback'            => array($this, 'delete_item'),
+        'permission_callback' => array($this, 'delete_item_permissions_check'),
         'args'                => array(
           'force' => array(
             'default' => false,
           ),
         ),
       ),
-    ) );
-    register_rest_route( $namespace, '/' . $base . '/schema', array(
+    ));
+    register_rest_route($namespace, '/' . $base . '/schema', array(
       'methods'  => WP_REST_Server::READABLE,
-      'callback' => array( $this, 'get_public_item_schema' ),
-    ) );
+      'callback' => array($this, 'get_public_item_schema'),
+    ));
   }
- 
+
   /**
    * Get a collection of items
    *
    * @param WP_REST_Request $request Full data about the request.
    * @return WP_Error|WP_REST_Response
    */
-  public function get_items( $request ) {
+  public function get_items($request)
+  {
     $items = array(); // DFDF do a query, call another class, etc
     $data = array();
-    foreach( $items as $item ) {
-      $itemdata = $this->prepare_item_for_response( $item, $request );
-      $data[] = $this->prepare_response_for_collection( $itemdata );
+    foreach ($items as $item) {
+      $itemdata = $this->prepare_item_for_response($item, $request);
+      $data[] = $this->prepare_response_for_collection($itemdata);
     }
- 
-    return new WP_REST_Response( $data, 200 );
+
+    return new WP_REST_Response($data, 200);
   }
- 
+
   /**
    * Get one item from the collection
    *
    * @param WP_REST_Request $request Full data about the request.
    * @return WP_Error|WP_REST_Response
    */
-  public function get_item( $request ) {
+  public function get_item($request)
+  {
     //get parameters from request
     $params = $request->get_params();
-    $item = array();// DFDF do a query, call another class, etc
-    $data = $this->prepare_item_for_response( $item, $request );
- 
+    $event_id = $params['id'];
+
+    // query the database
+    global $wpdb;
+    $table_name = $wpdb->prefix . "workshops";
+
+    $sql = $wpdb->prepare("SELECT * FROM `$table_name` WHERE id = %d", $event_id);
+    $workshop = $wpdb->get_row($sql, ARRAY_A);
+
+    $item = $workshop;
+
+    $data = $this->prepare_item_for_response($item, $request);
+
     //return a response or error based on some conditional
-    if ( 1 == 1 ) {
-      return new WP_REST_Response( $data, 200 );
+    if (1 == 1) {
+      return new WP_REST_Response($data, 200);
     } else {
-      return new WP_Error( 'code', __( 'message', 'text-domain' ) );
+      return new WP_Error('code', __('message', 'text-domain'));
     }
   }
- 
+
   /**
    * Create one item from the collection
    *
    * @param WP_REST_Request $request Full data about the request.
    * @return WP_Error|WP_REST_Response
    */
-  public function create_item( $request ) {
-    $item = $this->prepare_item_for_database( $request );
- 
-    if ( function_exists( 'slug_some_function_to_create_item' ) ) {
-      $data = slug_some_function_to_create_item( $item );
-      if ( is_array( $data ) ) {
-        return new WP_REST_Response( $data, 200 );
+  public function create_item($request)
+  {
+    $item = $this->prepare_item_for_database($request);
+
+    if (function_exists('slug_some_function_to_create_item')) {
+      $data = slug_some_function_to_create_item($item);
+      if (is_array($data)) {
+        return new WP_REST_Response($data, 200);
       }
     }
- 
-    return new WP_Error( 'cant-create', __( 'message', 'text-domain' ), array( 'status' => 500 ) );
+
+    return new WP_Error('cant-create', __('message', 'text-domain'), array('status' => 500));
   }
- 
+
   /**
    * Update one item from the collection
    *
    * @param WP_REST_Request $request Full data about the request.
    * @return WP_Error|WP_REST_Response
    */
-  public function update_item( $request ) {
-    $item = $this->prepare_item_for_database( $request );
- 
-    if ( function_exists( 'slug_some_function_to_update_item' ) ) {
-      $data = slug_some_function_to_update_item( $item );
-      if ( is_array( $data ) ) {
-        return new WP_REST_Response( $data, 200 );
+  public function update_item($request)
+  {
+    $item = $this->prepare_item_for_database($request);
+
+    if (function_exists('slug_some_function_to_update_item')) {
+      $data = slug_some_function_to_update_item($item);
+      if (is_array($data)) {
+        return new WP_REST_Response($data, 200);
       }
     }
- 
-    return new WP_Error( 'cant-update', __( 'message', 'text-domain' ), array( 'status' => 500 ) );
+
+    return new WP_Error('cant-update', __('message', 'text-domain'), array('status' => 500));
   }
- 
+
   /**
    * Delete one item from the collection
    *
    * @param WP_REST_Request $request Full data about the request.
    * @return WP_Error|WP_REST_Response
    */
-  public function delete_item( $request ) {
-    $item = $this->prepare_item_for_database( $request );
- 
-    if ( function_exists( 'slug_some_function_to_delete_item' ) ) {
-      $deleted = slug_some_function_to_delete_item( $item );
-      if ( $deleted ) {
-        return new WP_REST_Response( true, 200 );
+  public function delete_item($request)
+  {
+    $item = $this->prepare_item_for_database($request);
+
+    if (function_exists('slug_some_function_to_delete_item')) {
+      $deleted = slug_some_function_to_delete_item($item);
+      if ($deleted) {
+        return new WP_REST_Response(true, 200);
       }
     }
- 
-    return new WP_Error( 'cant-delete', __( 'message', 'text-domain' ), array( 'status' => 500 ) );
+
+    return new WP_Error('cant-delete', __('message', 'text-domain'), array('status' => 500));
   }
- 
+
   /**
    * Check if a given request has access to get items
    *
    * @param WP_REST_Request $request Full data about the request.
    * @return WP_Error|bool
    */
-  public function get_items_permissions_check( $request ) {
-    // return true; // <-- readable by all
-    return current_user_can( 'read_private_posts' );
+  public function get_items_permissions_check($request)
+  {
+    // DFDF:  TODO: proper authentication
+    return true; // <-- readable by all
+    // return current_user_can( 'read_private_posts' );
   }
- 
+
   /**
    * Check if a given request has access to get a specific item
    *
    * @param WP_REST_Request $request Full data about the request.
    * @return WP_Error|bool
    */
-  public function get_item_permissions_check( $request ) {
-    return $this->get_items_permissions_check( $request );
+  public function get_item_permissions_check($request)
+  {
+    return $this->get_items_permissions_check($request);
   }
- 
+
   /**
    * Check if a given request has access to create items
    *
    * @param WP_REST_Request $request Full data about the request.
    * @return WP_Error|bool
    */
-  public function create_item_permissions_check( $request ) {
-    return current_user_can( 'edit_post' );
+  public function create_item_permissions_check($request)
+  {
+    return current_user_can('edit_post');
   }
- 
+
   /**
    * Check if a given request has access to update a specific item
    *
    * @param WP_REST_Request $request Full data about the request.
    * @return WP_Error|bool
    */
-  public function update_item_permissions_check( $request ) {
-    return $this->create_item_permissions_check( $request );
+  public function update_item_permissions_check($request)
+  {
+    return $this->create_item_permissions_check($request);
   }
- 
+
   /**
    * Check if a given request has access to delete a specific item
    *
    * @param WP_REST_Request $request Full data about the request.
    * @return WP_Error|bool
    */
-  public function delete_item_permissions_check( $request ) {
-    return $this->create_item_permissions_check( $request );
+  public function delete_item_permissions_check($request)
+  {
+    return $this->create_item_permissions_check($request);
   }
- 
+
   /**
    * Prepare the item for create or update operation
    *
    * @param WP_REST_Request $request Request object
    * @return WP_Error|object $prepared_item
    */
-  protected function prepare_item_for_database( $request ) {
+  protected function prepare_item_for_database($request)
+  {
     return array();
   }
- 
+
   /**
    * Prepare the item for the REST response
    *
@@ -221,16 +243,18 @@ class WorkshopRestController extends WP_REST_Controller {
    * @param WP_REST_Request $request Request object.
    * @return mixed
    */
-  public function prepare_item_for_response( $item, $request ) {
-    return array();
+  public function prepare_item_for_response($item, $request)
+  {
+    return $item;
   }
- 
+
   /**
    * Get the query params for collections
    *
    * @return array
    */
-  public function get_collection_params() {
+  public function get_collection_params()
+  {
     return array(
       'page'     => array(
         'description'       => 'Current page of the collection.',
