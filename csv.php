6faef740222dff15ba85e6777bde383db7215ffc
Darryl F <darryl@tzap.ca>
2021-12-23 11:57:05 -0800

    Don't hide workshop ids, so we can correlate between the two tables.

--- csv.php
+++ csv.php
@@ -21,14 +21,12 @@ header("Content-Disposition: attachment; filename=$Filename");
 
 
 // verify nonce and user permissions
-if (! wp_verify_nonce($_POST['export_nonce'], 'export_nonce') ) {
+if (!wp_verify_nonce($_POST['export_nonce'], 'export_nonce')) {
 
 	echo '"security failure", "nonce"';
-
-} else if (! current_user_can('read_private_pages')) { 
+} else if (!current_user_can('read_private_pages')) {
 
 	echo '"security failure", "user cannot view private pages"';
-
 } else {
 
 	$to = $_POST['to'];
@@ -37,17 +35,17 @@ if (! wp_verify_nonce($_POST['export_nonce'], 'export_nonce') ) {
 	// transform dates.
 	$time = strtotime($from);
 	if ($time > 0) {
-		$from = date( 'Y-m-d', $time );
+		$from = date('Y-m-d', $time);
 	} else {
 		$from = "0000-00-00";
 	}
 	$time = strtotime($to);
 	if ($time > 0) {
-		$to = date( 'Y-m-d', $time );
+		$to = date('Y-m-d', $time);
 	} else {
-		$to = date( 'Y-m-d' );
+		$to = date('Y-m-d');
 	}
- 
+
 	// sql query.
 	global $wpdb;
 
@@ -58,9 +56,11 @@ if (! wp_verify_nonce($_POST['export_nonce'], 'export_nonce') ) {
 
 	if ($workshop_export) {
 
- 		$hiddenColumns = array("id");
-                $fancyColumnTitles = array("donationsdeposited" => "Donations Deposited", 
-			"chequenumber" => "Cheque Number");
+		// $hiddenColumns = array("id");
+		$fancyColumnTitles = array(
+			"donationsdeposited" => "Donations Deposited",
+			"chequenumber" => "Cheque Number"
+		);
 
 		$table_name = $wpdb->prefix . "workshops";
 
@@ -68,7 +68,7 @@ if (! wp_verify_nonce($_POST['export_nonce'], 'export_nonce') ) {
 		$sql = "SELECT column_name 'Column Name'
 			FROM information_schema.columns
 			WHERE table_name = '$table_name'";
-		$columns = $wpdb->get_results( $sql, ARRAY_A );
+		$columns = $wpdb->get_results($sql, ARRAY_A);
 
 		// data
 		$sql = $wpdb->prepare("
@@ -77,23 +77,23 @@ if (! wp_verify_nonce($_POST['export_nonce'], 'export_nonce') ) {
 					AND `date` <= %s ORDER BY `date`
 					", $from, $to);
 
-		$rows = $wpdb->get_results( $sql, ARRAY_A );
-
+		$rows = $wpdb->get_results($sql, ARRAY_A);
 	} else {
 		// attendance
 
- 		$hiddenColumns = array("id", "workshopid");
+		// $hiddenColumns = array("id", "workshopid");
+		$hiddenColumns = array("id");
 
 		$workshop_table = $wpdb->prefix . "workshops";
 		$attendance_table = $wpdb->prefix . "workshop_attendance";
 
 		// Titles
-		$columns = array( array("Column Name" => "Date") );
+		$columns = array(array("Column Name" => "Date"));
 
 		$sql = "SELECT column_name 'Column Name'
 			FROM information_schema.columns
 			WHERE table_name = '$attendance_table'";
-		$columns = array_merge($columns, $wpdb->get_results( $sql, ARRAY_A ));
+		$columns = array_merge($columns, $wpdb->get_results($sql, ARRAY_A));
 
 		$sql = $wpdb->prepare("
 			SELECT $workshop_table.date, $attendance_table . * 
@@ -104,16 +104,15 @@ if (! wp_verify_nonce($_POST['export_nonce'], 'export_nonce') ) {
 		        ORDER BY $workshop_table.date
 			", $from, $to);
 
-		$rows = $wpdb->get_results( $sql, ARRAY_A );
-
+		$rows = $wpdb->get_results($sql, ARRAY_A);
 	}
 
 	$sep = "";
-	foreach($columns as $column) {
+	foreach ($columns as $column) {
 		$column_name = $column['Column Name'];
-		if (! in_array($column_name, $hiddenColumns)) {
+		if (!in_array($column_name, $hiddenColumns)) {
 			$column_title = isset($fancyColumnTitles[$column_name]) ? $fancyColumnTitles[$column_name] : null;
-			if (! $column_title) {
+			if (!$column_title) {
 				$column_title = ucwords($column_name);
 			}
 
@@ -127,19 +126,18 @@ if (! wp_verify_nonce($_POST['export_nonce'], 'export_nonce') ) {
 	// iterate over rows
 	foreach ($rows as $row) {
 		$sep = "";
-		foreach($row as $column_name=>$field) {
-			if (! in_array($column_name, $hiddenColumns)) {
+		foreach ($row as $column_name => $field) {
+			if (!in_array($column_name, $hiddenColumns)) {
 				echo $sep;
 
-  				// handle NULL
-                                if ($field != NULL) {
-                                        // escape " character in field
-                                        $field = str_replace("\"", "\"\"", $field);
-                                        // strip newlines in field
-                                        $field = str_replace(array('\n', '\r'), " ", $field);
-
-                                }
-                                echo "\"" . stripslashes($field) . "\"";
+				// handle NULL
+				if ($field != NULL) {
+					// escape " character in field
+					$field = str_replace("\"", "\"\"", $field);
+					// strip newlines in field
+					$field = str_replace(array('\n', '\r'), " ", $field);
+				}
+				echo "\"" . stripslashes($field) . "\"";
 				$sep = ",";
 			}
 		}
